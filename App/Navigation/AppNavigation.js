eb60e7a95b4374d83aeb773b842f803e2283721e
Kevin VanGelder <kevin@infinite.red>
2017-05-29 16:59:23 -0700

    removed unused devscreen references from navigation

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,18 +1,11 @@
 import { StackNavigator } from 'react-navigation'
 import TextablesScreen from '../Containers/TextablesScreen'
-import LaunchScreen from '../Containers/LaunchScreen'
-import LoginScreen from '../Containers/LoginScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
-  TextablesScreen: { screen: TextablesScreen },
-  LaunchScreen: { screen: LaunchScreen },
-  LoginScreen: {
-    screen: LoginScreen,
-    navigationOptions: { title: 'Login' }
-  }
+  TextablesScreen: { screen: TextablesScreen }
 }, {
   // Default config for all screens
   headerMode: 'none',
